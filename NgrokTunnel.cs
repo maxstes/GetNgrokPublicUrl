a85f92c1e56443e60fe3f2019adee9e1fb721447
maxstes <maxstes479@gmail.com>
2023-12-11 17:04:47 +0200

    Add project files.

---
+++ NgrokTunnel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GetNgrokUrlTunnel
+{
+
+    public class NgrokTunnel
+    {
+        public List<TunnelDetail>? Tunnels { get; set; }
+    }
+
+    public class TunnelDetail
+    {
+        public string? name { get; set; }
+        public string? ID { get; set; }
+        public string? uri { get; set; }
+        public string? public_url { get; set; }
+        public string? proto { get; set; }
+        public ConfigDetail? config { get; set; }
+    }
+
+    public class ConfigDetail
+    {
+        public string? addr { get; set; }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GetNgrokUrlTunnel;
+using Newtonsoft.Json;
+
+class Program
+{
+    static HttpClient client = new HttpClient();
+
+    static async Task Main(string[] args)
+
+    {
+        //URL Web Interface
+        string ngrokApiUrl = "http://127.0.0.1:4040/api/tunnels";
+        try
+        {
+            HttpResponseMessage? response = await client.GetAsync(ngrokApiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                NgrokTunnel? ngrokTunnel = JsonConvert.DeserializeObject<NgrokTunnel>(responseBody);
+
+                foreach (var tunnel in ngrokTunnel.Tunnels)
+                {
+                    Console.WriteLine($"Name: {tunnel.name}, Public URL: {tunnel.public_url}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+        catch (HttpRequestException ex)
+        {
+            //Maybe you forgot to start ngok
+            await Console.Out.WriteLineAsync(ex.Message);
+        }
+    }
+}
